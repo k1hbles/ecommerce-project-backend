8e17c058074e648b4df3f8bde6883f49fa9dd7e4
hblk1e <63846874+k1hbles@users.noreply.github.com>
2026-06-24 06:50:52 +0000

    added stripe and checkout to index.js

--- index.js
+++ index.js
@@ -13,13 +13,17 @@ app.use(cors());
 const listingRouter = require("./routes/listings");
 const userRouter = require("./routes/users");
 const cartRouter = require("./routes/cart");
-const orderRouter = require("./routes/orders")
+const orderRouter = require("./routes/orders");
+const checkoutRouter = require("./routes/checkout");
+const stripeRouter = require("./routes/stripe");
 
 // Register routers
 app.use("/api/listings", [express.json()], listingRouter);
 app.use("/api/users", [express.json()], userRouter);
 app.use("/api/cart", [express.json()], cartRouter);
 app.use("/api/orders", [express.json()], orderRouter);
+app.use("/api/checkout", [express.json()], checkoutRouter);
+app.use("/api/stripe", stripeRouter);
 
 // Routes
 app.get('/', (req, res) => {
